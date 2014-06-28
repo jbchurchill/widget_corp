43024dfd772f2e978e60cc92969e89440d2604ac
JB <jchurchi@gmail.com>
2014-06-27 21:38:46 -0400

    added content to staff.php but no css styles yet

--- staff.php
+++ staff.php
@@ -1,8 +1,31 @@
 <html>
   <head>
-    <title>Basic</title>
+    <title>Widget Corp</title>
   </head>
   <body>
-
+    <div id="header">
+      <h1>Widget Corp</h1>
+    </div>
+    <div id="main">
+      <table id="structure">
+        <tr>
+          <td id="navigation">
+            &nbsp;
+          </td>
+          <td id="page">
+            <h2>Staff Menu</h2>
+            <p>Welcome to the staff area</p>
+            <ul>
+              <li><a href="content.php">Manage Website Content</a></li>
+              <li><a href="new_user.php">Add Staff User</a></li>
+              <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </td>
+        </tr>
+      </table>    
+    </div>
+    <div id="footer">
+      Copyright 2007, Widget Corp
+    </div>
   </body>
 </html>
