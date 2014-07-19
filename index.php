0d1edf95e387047f5511850c551b765205ad6efe
JB <jchurchi@gmail.com>
2014-07-19 12:50:43 -0400

    My working version of index.php at 3min 40sec into Chapter 14 (before Kevin goes over what he did).

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
 <?php
   find_selected_page();
 ?>
+<?php
+  if (intval($_GET['subj']) > 0) {
+    $subject_id = mysql_prep($_GET['subj']);
+    $subject = get_subject_by_id($subject_id);
+    $page_id = false;
+  } elseif (intval($_GET['page']) > 0) {
+    $page_id = mysql_prep($_GET['page']);
+    $page = get_page_by_id($page_id);
+    $subject_id = false;
+  } elseif ((intval($_GET['subj']) == 0) && (intval($_GET['page']) == 0)) {
+    $subject_id = false;
+    $page_id = false;
+    // redirect_to("content.php");
+    // NOT SURE I WANT TO DO THIS REDIRECT SINCE WE ARE IN THE PUBLIC AREA
+  } else {
+    // hmmm not sure what is left after that
+  }
+  
+?>
 <?php include("includes/header.php"); ?>
 <table id="structure">
   <tr>
@@ -15,7 +34,27 @@
     </td>
     <td id="page">
       <div id="page-content">
-      WHATS UP ?
+        <?php
+          if ($subject_id) {
+            echo "<h2>Subject: " . $subject['menu_name'] . " - (ID: " . $subject_id . ")</h2>";
+            echo "Position: " . $subject['position'] . "<br />";
+            echo "Visible: " . $subject['visible'] . "<br /><br />";
+            echo "Pages: <br /><br /><ul>";
+            $page_set = get_all_pages_for_subject($subject_id);
+            if ($page_set) {
+              while ($pagelisted = mysql_fetch_array($page_set)) {
+                echo "<li><a href=\"index.php?page=" . urlencode($pagelisted['position']) . "\">" . $pagelisted['menu_name'] . "</a></li>"; 
+              }
+            }
+            echo "</ul>";
+          }
+          if ($page_id) {
+            echo "<h2>Page: " . $page['menu_name'] . " - (ID: " . $page_id . ")</h2>";
+            echo "Position: " . $page['position'] . "<br />";
+            echo "Visible: " . $page['visible'] . "<br /><br />";
+            echo "Content: <br /><br /><p>" . $page['content'] . "</p>";
+          }
+        ?>
       </div>
     </td>
   </tr>
